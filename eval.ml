c544ab2c4421c82f2b8548bd1d48c0763e557161
Flavio <flavio.a.618@gmail.com>
2021-04-23 15:35:34 +0200

    Completed implementation of Fetch up to some things to discuss

--- eval.ml
+++ eval.ml
@@ -108,6 +108,14 @@ class fresh_factory =
 
 let fresh = new fresh_factory;;
 
+(* Get a fresh variable name. Use x as the base, to highlight that it
+   holds a temporary value of variable x *)
+let get_fresh_name (rho : value StringMap.t) (x : identifier) : identifier =
+  let rec _get_fresh (y : identifier) =
+    let res = y ^ ("'") in
+      if StringMap.mem res rho then res else _get_fresh res
+  in _get_fresh x
+
 let stepFetch (conf : configuration) : (configuration * observation) option =
   match conf.cs with
     | Skip :: cs1 -> some ({conf with cs = cs1; is = conf.is @ [Nop]}, None)
@@ -120,12 +128,47 @@ let stepFetch (conf : configuration) : (configuration * observation) option =
       -> let guard_expr = BinOp(idx_expr, Length(Cst(CstA(a))), Lt)
          and then_cmd = VarAssign(ide, PtrRead(BinOp(Base(Cst(CstA(a))), idx_expr, Add), a.label)) in
           some ({conf with cs = If(guard_expr, then_cmd, Fail) :: cs1}, None)
-    (* | PtrAssign of expr * expr * label
-    | ArrAssign of arr * expr * expr
-    | Seq of cmd * cmd
-    | If of expr * cmd * cmd
-    | While of expr * cmd
-    | Protect of identifier * protect * rhs;; *)
+    | PtrAssign(addr_expr, expr, l) :: cs1
+      -> some ({conf with cs = cs1; is = conf.is @ [StoreE(addr_expr, l, expr)]}, None)
+    | ArrAssign(a, idx_expr, expr) :: cs1
+      -> let guard_expr = BinOp(idx_expr, Length(Cst(CstA(a))), Lt)
+         and then_cmd = PtrAssign(BinOp(Base(Cst(CstA(a))), idx_expr, Add), expr, a.label) in
+          some ({conf with cs = If(guard_expr, then_cmd, Fail) :: cs1}, None)
+    | Seq(c1, c2) :: cs1 -> some ({conf with cs = c1 :: c2 :: cs1}, None)
+    | While(guard_expr, body_cmd) :: cs1
+      -> let c2 = Seq(body_cmd, While(guard_expr, body_cmd)) in
+          some ({conf with cs = If(guard_expr, c2, Skip) :: cs1}, None)
+    (* protect *)
+    | Protect(ide, Slh, ArrayRead(a, idx_expr)) :: cs1
+      -> let e1 = BinOp(idx_expr, Length(Cst(CstA(a))), Lt)
+         and e2 = BinOp(Base(Cst(CstA(a))), idx_expr, Add)
+         and r = get_fresh_name conf.rho "r" in
+         let c1 = VarAssign(r, Expr(e1))
+         and c2 = VarAssign(r, Expr(InlineIf(Var(r), Cst(CstI(1)), Cst(CstI(0)))))
+         and c3 = VarAssign(ide, PtrRead(BinOp(e2, Var(r), BitAnd), a.label)) in
+         let c' = Seq(c1, If(Var(r), Seq(c2, c3), Fail)) in
+          some ({conf with cs = c' :: cs1}, None)
+    | Protect(ide, prot_kind, Expr(expr)) :: cs1
+      -> some ({conf with cs = cs1; is = conf.is @ [IProtectE(ide, prot_kind, expr)]}, None)
+    | Protect(ide, prot_kind, PtrRead(addr_expr, l)) :: cs1
+      -> let x' = get_fresh_name conf.rho ide in
+         let c1 = VarAssign(x', PtrRead(addr_expr, l))
+         and c2 = Protect(ide, prot_kind, Expr(Var(x'))) in
+          some ({conf with cs = c1 :: c2 :: cs1}, None)
+    | Protect(ide, prot_kind, ArrayRead(a, idx_expr)) :: cs1
+      -> let x' = get_fresh_name conf.rho ide in
+         let c1 = VarAssign(x', ArrayRead(a, idx_expr))
+         and c2 = Protect(ide, prot_kind, Expr(Var(x'))) in
+          some ({conf with cs = c1 :: c2 :: cs1}, None)
+    | _ -> printf "unexpected command for fetch, ignoring"; none
+
+let stepPFetch (pred : prediction) (conf : configuration) : (configuration * observation) option =
+  match conf.cs with
+    | If(guard_expr, then_cmd, else_cmd) :: cs1
+      -> let taken = if pred then then_cmd else else_cmd
+         and not_taken = if pred then else_cmd else then_cmd in
+          some ({conf with cs = taken :: cs1; is = conf.is @ [Guard(guard_expr, pred, not_taken :: cs1, fresh#get)]}, None)
+    | _ -> printf "unexpected command for pfetch, ignoring"; none
 
 let step (conf : configuration) (d : directive) : (configuration * observation) option =
   match conf.is, conf.cs, d with
